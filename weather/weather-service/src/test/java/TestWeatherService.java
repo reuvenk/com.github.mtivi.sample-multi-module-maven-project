addf5d894e3f205667b33774b1316e79e16bd9dc
Matthew <matthewirving1990@gmail.com>
2021-04-03 10:38:39 -0400

    Modify service only

--- weather/weather-service/src/test/java/TestWeatherService.java
+++ weather/weather-service/src/test/java/TestWeatherService.java
@@ -1,7 +1,28 @@
 import com.github.mtivi.NotifyTestsRunning;
+import com.github.mtivi.weather.WeatherService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestWeatherService extends NotifyTestsRunning {
-    public String getModuleName(){
+
+    WeatherService service = new WeatherService();
+
+    public String getModuleName() {
         return "weather-service";
+    }
+
+
+    @Test
+    @DisplayName("Test that forecast dates in the past throw an exception")
+    void testPastDate() {
+        assertEquals("Historical weather does not need a forecast.", assertThrows(IllegalArgumentException.class,
+                () -> service.getForecast(LocalDateTime.now().minusSeconds(1)))
+                .getMessage());
+
     }
 }
