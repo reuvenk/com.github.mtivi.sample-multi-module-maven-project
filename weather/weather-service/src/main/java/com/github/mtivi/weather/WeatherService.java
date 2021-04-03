6c3e10c4b10219d3d1a156d017f7ece3d1f72c5f
Matthew <matthewirving1990@gmail.com>
2021-04-03 09:35:00 -0400

    Modify Service only.

--- weather/weather-service/src/main/java/com/github/mtivi/weather/WeatherService.java
+++ weather/weather-service/src/main/java/com/github/mtivi/weather/WeatherService.java
@@ -1,12 +1,15 @@
 package com.github.mtivi.weather;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class WeatherService {
 
     private final List<Weather> weathers = new ArrayList<>(List.of(Weather.values()));
+    private final static Random random = new Random(System.currentTimeMillis());
 
     private final Weather randomWeather() {
         Collections.shuffle(weathers);
@@ -15,5 +18,12 @@ public class WeatherService {
 
     public Weather currentWeather() {
         return randomWeather();
+    }
+
+    public Forecast getForecast(LocalDateTime localDateTime) {
+        return Forecast.builder()
+                .weather(randomWeather())
+                .probability(random.nextInt(100))
+                .build();
     }
 }
